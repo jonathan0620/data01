5313dffd8b505e246fbf5c647b2cf853c81a50c3
jonathan <0620jonathan@gmail.com>
2023-11-21 10:15:41 +0900

    랜덤 배열 수정

--- src/배열응용/랜덤한배열최대값찾기.java
+++ src/배열응용/랜덤한배열최대값찾기.java
@@ -1,5 +1,6 @@
 package 배열응용;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import 배열기본.Print;
@@ -11,23 +12,17 @@ public class 랜덤한배열최대값찾기 {
 	//2. 최대값 구하기
 	//3. 최대값의 갯수는?
 	public static void main(String[] args) {
-		int r = numbers[0];
-		r.setSeed(100);
-		int[] numbers = new int[1000];
-		
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = r.nextInt(1000);
+		int[] s = new int[1000]; 
+		Random r = new Random(100); //seed, 씨앗값!
+		for (int i = 0; i < s.length; i++) {
+			s[i] = r.nextInt(999) + 1; //0~998--> 1~999
 		}
-		
-		Print.arr(numbers);
-		
-		int max = numbers[0];
-		
-		for (int i = 0; i < numbers.length; i++) {
-			if (max < 1000) {
-				max;
-			}
-		}
-		
-		System.out.println(max);
+		System.out.println(Arrays.toString(s));
+		//정렬후 최소/최대 
+		Arrays.sort(s); //오름차순 정렬, 파괴함수
+		//<---> 비파괴함수, String num = "100"; Integer.parseInt(num); 
+		System.out.println(Arrays.toString(s)); //출력해서 확인!
+		System.out.println("최소값>> "+ s[0]);
+		System.out.println("최대값>> "+ s[s.length - 1]);
+	}
 }
